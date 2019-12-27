39ceb675527dac13d2211df8d6c08d8acc07fdf4
v_yuzhuang01 <v_yuzhuang01@baidu.com>
2019-12-27 15:39:22 +0800

    ini

---
+++ src/Layout/CommonLayout/index.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+export default class Layout extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        const {
+            children
+        } = this.props;
+
+        return (
+            <div>
+                {children}
+            </div>
+        )
+    }
+}
